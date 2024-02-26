9e09dd556eedb749d8194168455b0038d65ac2d3
DaveAlsina <davealsina@gmail.com>
2024-02-26 16:06:34 -0500

    Update secrets.h

--- micropython/friendlyCode/archetype_cpp_version/main/secrets.h
+++ micropython/friendlyCode/archetype_cpp_version/main/secrets.h
@@ -7,6 +7,8 @@
 
 extern const String API_URL  = "192.168.1.133"; 
 extern const uint   API_PORT = 8080;
+
+#define HTTP_TIMEOUT 7500
 #define API_ENDPOINT "/api/data"
 #define API_USERNAME "admin"
 #define API_PASSWORD "admin"
